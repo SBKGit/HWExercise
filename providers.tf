f2472df949bcd718b910895df474ddfdd59e7483
Shireesh Kantharaj <52453070+SBKGit@users.noreply.github.com>
2023-01-15 12:27:17 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,13 @@
-provider "aws"{
-  region  = var.aws_region
-  version = "2.60.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+# provider "aws"{
+#   region  = var.aws_region
+#   version = "2.60.0"
+# }
